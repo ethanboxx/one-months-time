2004c67ae75a696d9f667612bf9ab9a2a77a3a8b
Ethan Brierley <ethanboxx@gmail.com>
2020-03-18 16:33:57 +0000

    remove test

--- server/src/app.controller.spec.ts
+++ server/src/app.controller.spec.ts
@@ -1,20 +1,5 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AppController } from './app.controller';
-
-describe('AppController', () => {
-  let appController: AppController;
-
-  beforeEach(async () => {
-    const app: TestingModule = await Test.createTestingModule({
-      controllers: [AppController],
-    }).compile();
-
-    appController = app.get<AppController>(AppController);
-  });
-
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.findAll());
-    });
+describe('', () => {
+  it('', () => {
+    expect(1);
   });
 });
