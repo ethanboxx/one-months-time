307636704cab423925f4e7ab9a40de4ac1850cfd
Ethan Brierley <ethanboxx@gmail.com>
2020-03-19 10:34:42 +0000

    Nice animation

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <h1>One Month's Time</h1>
-    <h2>Hold your future self accountable.</h2>
+    <header>
+      <h1>One Month's Time</h1>
+      <h2>Hold your future self accountable.</h2>
+    </header>
     <form>
       <label for="email">First Name</label>
       <input type="email" id="email" name="email" placeholder="Your Email.." required />
@@ -34,8 +36,10 @@ body
   min-height: 100vh
   margin: 0
   padding: 0
-  padding-top: 30px
   box-sizing: border-box
+  display: flex
+  justify-content: center
+  align-items: center
   *,
   *::after,
   *::before
@@ -57,13 +61,33 @@ textarea, input, button, select
 #app
   max-width: 1000px
   padding: 50px
-  margin: 0 auto
   backdrop-filter: blur(10px) brightness(0.8)
   border-radius: 7px
   background-color: rgba(255, 255,255 ,0.3 )
   box-shadow: 0px 0px 9px -1px rgba(0,0,0,0.75)
+  border: 3px inset rgba(255,255,255, .2)
+  animation: glass
+  animation-duration: 1s
+  animation-timing-function: ease-out
 
-h1
-  margin: 0
-  padding: 10px
+header
+  h1
+    margin: 0
+    margin-bottom: -10px
+    font-size: 3.5em
+    padding: 0
+    font-family: 'Baloo Da 2', cursive
+
+  h2
+    margin-top: 0
+
+@keyframes glass
+  0%
+    backdrop-filter: blur(0.1px) brightness(0.9)
+    box-shadow: 0px 0px 0px -1px rgba(0,0,0,1)
+    transform: scale(0.98)
+    filter: blur(1px)
+  100%
+    backdrop-filter: blur(10px) brightness(0.8)
+    box-shadow: 0px 0px 9px -1px rgba(0,0,0,0.75)
 </style>
